01a24d7695680dbe66a542412182fa4cf8da5c52
Roooooobin <lz2294@nyu.edu>
2022-04-10 12:53:56 -0400

    init

---
+++ init_db.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func getDB() *gorm.DB {
+
+	username := "root"
+	password := "19980506"
+	host := "127.0.0.1"
+	port := 3306
+	dbName := "MyStackoverflow"
+	timeout := "10s"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s",
+		username, password, host, port, dbName, timeout)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("connection failed, error=" + err.Error())
+	}
+	return db
+}
