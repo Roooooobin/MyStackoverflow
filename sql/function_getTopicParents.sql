54fef0f3737b5598a416da44fa62b971c8eda557
Roooooobin <lz2294@nyu.edu>
2022-04-15 20:14:24 -0400

    modify function getTopicParents

---
+++ sql/function_getTopicParents.sql
@@ -0,0 +1,12 @@
+# function to get parent topics in recursive way
+CREATE DEFINER=`root`@`localhost` FUNCTION `getTopicParents`(root_id int) RETURNS varchar(1000) CHARSET utf8mb4
+BEGIN
+    DECLARE res VARCHAR(1000) DEFAULT '$';
+    DECLARE par int DEFAULT (SELECT parent_tid FROM TopicHierarchy WHERE tid = root_id);
+    WHILE root_id != par DO
+            SET res = CONCAT(res, ',', par);
+            SET root_id = par;
+            SET par = (SELECT parent_tid FROM TopicHierarchy WHERE tid = par);
+        END WHILE;
+    RETURN res;
+END
