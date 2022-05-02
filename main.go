253d388b1219565be9319b454b885c499eb36f61
Roooooobin <lz2294@nyu.edu>
2022-05-01 21:05:43 -0400

    move router to a separate file

--- main.go
+++ main.go
@@ -2,81 +2,10 @@ package main
 
 import (
 	"MyStackoverflow/cache"
-	"MyStackoverflow/handler/answer"
-	"MyStackoverflow/handler/keyword_search"
-	"MyStackoverflow/handler/question"
-	"MyStackoverflow/handler/topic"
-	"MyStackoverflow/handler/user"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	groupUser := r.Group("/user")
-	{
-		groupUser.POST("/add", func(c *gin.Context) {
-			user.AddUser(c)
-		})
-		groupUser.GET("/list", func(c *gin.Context) {
-			user.ListUser(c)
-		})
-		groupUser.POST("/edit", func(c *gin.Context) {
-			user.EditUser(c)
-		})
-	}
-
-	groupTopic := r.Group("/topic")
-	{
-		groupTopic.POST("/add", func(c *gin.Context) {
-			topic.AddTopic(c)
-		})
-	}
-
-	groupQuestion := r.Group("/question")
-	{
-		groupQuestion.POST("/add", func(c *gin.Context) {
-			question.AddQuestion(c)
-		})
-		groupQuestion.POST("/like", func(c *gin.Context) {
-			question.LikeQuestion(c)
-		})
-		groupQuestion.GET("/list", func(c *gin.Context) {
-			question.ListQuestion(c)
-		})
-		groupQuestion.POST("/edit", func(c *gin.Context) {
-			question.EditQuestion(c)
-		})
-	}
-
-	groupAnswer := r.Group("/answer")
-	{
-		groupAnswer.POST("/add", func(c *gin.Context) {
-			answer.AddAnswer(c)
-		})
-		groupAnswer.POST("/like", func(c *gin.Context) {
-			answer.LikeAnswer(c)
-		})
-		groupAnswer.GET("/list", func(c *gin.Context) {
-			answer.ListAnswer(c)
-		})
-		groupAnswer.POST("/select", func(c *gin.Context) {
-			answer.SelectBest(c)
-		})
-		groupAnswer.POST("/rate", func(c *gin.Context) {
-			answer.RateAnswer(c)
-		})
-		groupAnswer.POST("/edit", func(c *gin.Context) {
-			answer.EditAnswer(c)
-		})
-	}
-
-	groupKeyword := r.Group("/keyword_search")
-	{
-		groupKeyword.GET("/list", func(c *gin.Context) {
-			keyword_search.ListByKeyword(c)
-		})
-	}
-
+	r := RegisterRouter()
 	// pre-computed cache
 	cache.Init()
 	// listen and serve on 0.0.0.0:8080
---
+++ router.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"MyStackoverflow/handler/answer"
+	"MyStackoverflow/handler/keyword_search"
+	"MyStackoverflow/handler/question"
+	"MyStackoverflow/handler/topic"
+	"MyStackoverflow/handler/user"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRouter() *gin.Engine {
+
+	r := gin.Default()
+	groupUser := r.Group("/user")
+	{
+		groupUser.POST("/add", func(c *gin.Context) {
+			user.AddUser(c)
+		})
+		groupUser.GET("/list", func(c *gin.Context) {
+			user.ListUser(c)
+		})
+		groupUser.POST("/edit", func(c *gin.Context) {
+			user.EditUser(c)
+		})
+	}
+
+	groupTopic := r.Group("/topic")
+	{
+		groupTopic.POST("/add", func(c *gin.Context) {
+			topic.AddTopic(c)
+		})
+	}
+
+	groupQuestion := r.Group("/question")
+	{
+		groupQuestion.POST("/add", func(c *gin.Context) {
+			question.AddQuestion(c)
+		})
+		groupQuestion.POST("/like", func(c *gin.Context) {
+			question.LikeQuestion(c)
+		})
+		groupQuestion.GET("/list", func(c *gin.Context) {
+			question.ListQuestion(c)
+		})
+		groupQuestion.POST("/edit", func(c *gin.Context) {
+			question.EditQuestion(c)
+		})
+	}
+
+	groupAnswer := r.Group("/answer")
+	{
+		groupAnswer.POST("/add", func(c *gin.Context) {
+			answer.AddAnswer(c)
+		})
+		groupAnswer.POST("/like", func(c *gin.Context) {
+			answer.LikeAnswer(c)
+		})
+		groupAnswer.GET("/list", func(c *gin.Context) {
+			answer.ListAnswer(c)
+		})
+		groupAnswer.POST("/select", func(c *gin.Context) {
+			answer.SelectBest(c)
+		})
+		groupAnswer.POST("/rate", func(c *gin.Context) {
+			answer.RateAnswer(c)
+		})
+		groupAnswer.POST("/edit", func(c *gin.Context) {
+			answer.EditAnswer(c)
+		})
+	}
+
+	groupKeyword := r.Group("/keyword_search")
+	{
+		groupKeyword.GET("/list", func(c *gin.Context) {
+			keyword_search.ListByKeyword(c)
+		})
+	}
+	return r
+}
