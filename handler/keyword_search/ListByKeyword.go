e73a39563290f522f529c9e443c73fec3e56fb8d
Roooooobin <lz2294@nyu.edu>
2022-05-14 21:59:43 -0400

    fix keyword

--- handler/keyword_search/ListByKeyword.go
+++ handler/keyword_search/ListByKeyword.go
@@ -52,7 +52,9 @@ func ListByKeyword(c *gin.Context) {
 	keyword := c.Query("keyword")
 	// failed to resolve the problem(always a ",function%20()%20{%20[native%20code]%20}" attached at last) on frontend
 	keywords := strings.Split(keyword, ",")
-	keyword = strings.Join(keywords[:len(keywords)-1], ",")
+	if keywords[len(keywords)-1] == "function%20()%20{%20[native%20code]%20}" {
+		keyword = strings.Join(keywords[:len(keywords)-1], ",")
+	}
 	if !function.CheckNotEmpty(keyword) {
 		errMsg = "Input keyword can not be empty, please check and retry."
 		return
